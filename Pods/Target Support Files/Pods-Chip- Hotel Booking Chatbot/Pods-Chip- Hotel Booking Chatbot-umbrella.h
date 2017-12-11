4ccdfe585610f3f195526c9355a8dd0ce7f845a0
mobinius <narendra.v@mobinius.com>
2017-12-11 17:30:16 +0530

    added chatbot to ios app

---
+++ Pods/Target Support Files/Pods-Chip- Hotel Booking Chatbot/Pods-Chip- Hotel Booking Chatbot-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Chip__Hotel_Booking_ChatbotVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Chip__Hotel_Booking_ChatbotVersionString[];
+
